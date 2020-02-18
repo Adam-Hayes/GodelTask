431ff3f9cb5cb6eef0468c976c87ceaeca6b0548
Adam-Hayes <pustahanovdima@yandex.ru>
2020-02-18 04:42:23 +0300

    Update and rename dml_database_query.sql to dal_database_script.sql

---
+++ dal_database_script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE cinema;
+    
+	
+	Create table Director(
+	id serial primary key,
+	first_name character varying(255),
+	last_name character varying(255),
+	birth_date date	
+	);
+	
+	Create table Film(
+	id serial primary key,
+	director_id integer references Director(id), 
+	name character varying(255),
+	release_date date,
+	genre character varying(255)
+	)
